d8f36732a11047155a70879f0c3bcf326f388c95
onseas <osmarneto750@gmail.com>
2023-05-02 02:11:41 -0300

    serapando em funções

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        RSA cript = new RSA("Ola mundo!");
+        String m;
+        byte [] c;
+
+        RSA rsa = new RSA();
+        c = rsa.encript("Olá mundo", rsa.publicKey);
+        System.out.println("\n\nMensagem Cifrada: " + c);
+        m = rsa.decript(c);
+        System.out.println("Mensagem decriptada: " + m);
     }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -1,30 +1,37 @@
+import javax.crypto.Cipher;
 import java.security.*;
-import javax.crypto.*;
 
 public class RSA {
-    public RSA(String m) throws Exception {
+    private PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public RSA() throws Exception {
         // Gerar um par de chaves RSA
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(2048);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
-        System.out.println("public key: " + publicKey);
-        System.out.println("private key: " + privateKey);
-
-        // Criptografar uma mensagem usando a chave pública
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] mensagemCriptografada = cipher.doFinal(m.getBytes());
-        System.out.println("Mensagem criptografada: " + mensagemCriptografada);
-
-        // Descriptografar a mensagem usando a chave privada
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] mensagemDescriptografada = cipher.doFinal(mensagemCriptografada);
-        String mensagemFinal = new String(mensagemDescriptografada);
-
-        // Imprimir a mensagem original e a mensagem descriptografada
-        System.out.println("Mensagem original: " + m);
-        System.out.println("Mensagem descriptografada: " + mensagemFinal);
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA"); // Objeto usado para gerar chave, define algoritmo
+        keyPairGenerator.initialize(2048); // Define tamanho da chave
+        KeyPair keyPair = keyPairGenerator.generateKeyPair(); // Gera as chaves
+        publicKey = keyPair.getPublic(); //pega as chave publica
+        System.out.println("\npublic key: " + publicKey);
+        privateKey = keyPair.getPrivate();//pega a chave privada
+        System.out.println("\nprivate key: " + privateKey);
     }
+
+    byte[] encript(String m, PublicKey publicKey) throws Exception {
+        // Criptografar uma mensagem usando a chave pública
+        // A cifragem é feita com RSA, Electronic Codebook (divide em blocos e usa a mesma chave em todos) e PKCS1Padding para preencher os blocos se não tiver o suficiente.
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); // Definindo a maneira de criptografia
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey); // Ativa o modo de encriptação
+        byte[] mCifrada = cipher.doFinal(m.getBytes()); // Transforma a mensagem em bytes e encripta
+        return mCifrada;
+    }
+
+    String decript(byte[] mCifrada) throws Exception {
+        // Descriptografar a mensagem usando a chave privada
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); // Definindo a maneira de criptografia
+        cipher.init(Cipher.DECRYPT_MODE, privateKey); // Ativa o modo de decriptação
+        byte[] mDecriptada = cipher.doFinal(mCifrada); // Recebe a mensagem cifrada e decripta
+        String m = new String(mDecriptada);
+        return m;
+    }
+
 }
